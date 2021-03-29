4096e365272a20cd3dd41c179b7fc02031a2dc26
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2021-03-29 15:54:30 +0800

    更新：show 彈出協同者設定視窗

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -5,8 +5,13 @@
         <meta charset="utf-8">
 
         <title>顯示筆記(本人)</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     </head>
+
     <h1>顯示&編輯筆記　<button onclick="addeditor()"><i class="fas fa-plus"></i></button></h1>
 
     <div id="addpeo" style="display:none">
@@ -64,6 +69,30 @@
 
 
 
+    </div>
+
+
+    <button onclick="tttt()" type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModal">
+        測試測試
+    </button>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">協同者添加/刪除</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    這邊要放協同者
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">關閉</button>
+                </div>
+            </div>
+        </div>
     </div>
 
     <div style="display:none">
@@ -651,6 +680,12 @@
 //             }
         }
     }
+
+
+    function tttt(){
+
+    }
+
 
     // function invite(){
     //     const form = document.forms['inn'];
