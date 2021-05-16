7eefa628b28c0cfc63443c6f337947e5213df33b
benpeng <3a732078@gm.student.ncut.edu.tw>
2021-05-16 19:51:26 +0800

    微調header

--- resources/views/teacher/layouts/header.blade.php
+++ resources/views/teacher/layouts/header.blade.php
@@ -22,45 +22,29 @@
 
         <nav id="navbar" class="navbar">
             <ul>
-                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                <li><a class="nav-link scrollto" href="#about">About</a></li>
-                <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-                <li><a class="nav-link scrollto" href="#team">Team</a></li>
-                <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
-
                 <li class="dropdown">
                     <a href="#">
                         <span>
-                            Drop Down
+                            上學期
                         </span>
                         <i class="bi bi-chevron-down"></i>
                     </a>
-                    <ul>
-                        <li>
-                            <a href="#">Drop Down 1</a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#">
-                                <span>Deep Drop Down</span>
-                                <i class="bi bi-chevron-right">
-
-                                </i>
-                            </a>
-                            <ul>
-                                <li><a href="#">Deep Drop Down 1</a></li>
-                                <li><a href="#">Deep Drop Down 2</a></li>
-                                <li><a href="#">Deep Drop Down 3</a></li>
-                                <li><a href="#">Deep Drop Down 4</a></li>
-                                <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Drop Down 2</a></li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
-                    </ul>
                 </li>
-
+                <li>
+                    <a href="#">
+                        <span>
+                            下學期
+                        </span>
+                        <i class="bi bi-chevron-down"></i>
+                    </a>
+                </li>
+            </ul>
+            <select class="form-select" aria-label="Disabled select example" disabled>
+                    <option selected>課程</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
                 <li class="dropdown">
                     <span style="color: #9fcdff">使用者</span>
                     <i class="bi bi-chevron-right"></i>
@@ -69,9 +53,6 @@
                         <li><a href="#">登出</a></li>
                     </ul>
                 </li>
-            </ul>
-
-            <i class="bi bi-list mobile-nav-toggle">asdfasdfdasfdasfds</i>
         </nav>
         <!-- .navbar -->
     </div>
