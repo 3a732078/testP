de0973cd4e38f1341a9fdae8d411fc00b80d0df5
benpeng <3a732078@gm.student.ncut.edu.tw>
2021-05-17 20:04:26 +0800

    設定教授選擇課程的介面

--- resources/views/teacher/layouts/header.blade.php
+++ resources/views/teacher/layouts/header.blade.php
@@ -1,60 +1,33 @@
 
 <header id="header" class="fixed-top header-transparent">
     <div class="container d-flex align-items-center justify-content-between">
-        <h1 class="logo"><a href="index">@yield('headername')</a></h1>
-        <nav id="navbar" class="navbar">
-            <li class="dropdown">
-                <a href="#">
-                    <h3>
-                        110
-                    </h3>
-                    <i class="bi bi-chevron-down">學年度</i>
-                </a>
-                <ul>
-                    <li>
-                        <a href="#">Drop Down 1</a>
-                    </li>
-                </ul>
-            </li>
-        </nav>
-
+        <h1 class="logo">
+            <a href="index">
+                @yield('headername')
+            </a>
+        </h1>
         <!-- Uncomment below if you prefer to use an image logo -->
-
         <nav id="navbar" class="navbar">
             <ul>
-                <li class="dropdown">
-                    <a href="#">
-                        <span>
-                            上學期
-                        </span>
-                        <i class="bi bi-chevron-down"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span>
-                            下學期
-                        </span>
-                        <i class="bi bi-chevron-down"></i>
-                    </a>
-                </li>
-            </ul>
-            <select class="form-select" aria-label="Disabled select example" disabled>
-                    <option selected>課程</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
-                <li class="dropdown">
-                    <span style="color: #9fcdff">使用者</span>
-                    <i class="bi bi-chevron-right"></i>
+                {{--                顯示該教授當學期所有課程--}}
+                <li><a class="nav-link scrollto" href="#">Course</a></li>
+                <li><a class="nav-link scrollto" href="#">Course</a></li>
+                <li><a class="nav-link scrollto" href="#">Course</a></li>
+                <li><a class="nav-link scrollto" href="#">Course</a></li>
+                <li><a class="nav-link scrollto" href="#">Course</a></li>
+                <li><a class="nav-link scrollto" href="#">Course</a></li>
+                {{--                顯示該教授當學期所有課程--}}
+
+                {{--                登出等介面--}}
+                <li class="dropdown"><a href="#"><span>使用者</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
-                        <li><a href="#">設定</a></li>
-                        <li><a href="#">登出</a></li>
+                        <li><a href="#">Deep Drop Down 1</a></li>
+                        <li><a href="#">Deep Drop Down 2</a></li>
                     </ul>
                 </li>
-        </nav>
-        <!-- .navbar -->
+                {{--                登出等介面--}}
+            </ul>
+        </nav><!-- .navbar -->
+
     </div>
-</header>
-<!-- End Header -->
+</header><!-- End Header -->
