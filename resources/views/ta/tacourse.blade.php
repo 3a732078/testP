284e60be7185acf0d8c1b2fa8375340fd2103557
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2021-05-28 14:10:49 +0800

    更新:ta版面

--- resources/views/ta/tacourse.blade.php
+++ resources/views/ta/tacourse.blade.php
@@ -19,23 +19,6 @@
     </li>
 
     <!-- Nav Item - Utilities Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-               aria-expanded="true" aria-controls="collapseUtilities">
-                <i class="fas fa-fw fa-folder-open"></i>
-                <span>筆記專區</span>
-            </a>
-            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                 data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">筆記相關資訊:</h6>
-                    <a class="collapse-item" href="/notes/create">新增筆記</a>
-                    <a class="collapse-item" href="{{route('notes.mynotes')}}">我的筆記</a>
-                    <a class="collapse-item" href="#">搜尋筆記</a>
-                    <a class="collapse-item" href="#">收藏筆記</a>
-                </div>
-            </div>
-        </li>
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#textbooks"
