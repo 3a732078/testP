28ab4ebc4a91dcae734cf34c1c445cdfb97ab2cb
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2021-06-10 10:58:27 +0800

    更新: TA連絡學生版面

--- resources/views/questions/tashow.blade.php
+++ resources/views/questions/tashow.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts/tahome')
 <head>
 
     <meta charset="utf-8">
@@ -5,6 +6,51 @@
     <title>私訊(TA)-{{$classn}}</title>
 
 </head>
+@section('tanav')
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+           aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-book"></i>
+            <span>課程</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">已選課程:</h6>
+                {{--                @if ($count > 0)--}}
+                {{--                    @for($i = 0; $i < $count; $i++)--}}
+                {{--                        <a class="collapse-item" href="/ta/classes/@php echo $tacid[$i]; @endphp" >@php echo $tac[$i]; @endphp</a>--}}
+                {{--                    @endfor--}}
+                {{--                @endif--}}
+            </div>
+        </div>
+    </li>
+
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#textbooks"
+           aria-expanded="true" aria-controls="textbookss">
+            <i class="fas fa-fw fa-folder-open"></i>
+            <span>教材</span>
+        </a>
+        <div id="textbooks" class="collapse" aria-labelledby="textbooks"
+             data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">教材相關:</h6>
+                <a class="collapse-item" href="/textbooks/create">新增教材</a>
+                <a class="collapse-item" href="/textbooks">教材管理</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+
+
+
+@endsection
+
+@section('notice')
 <div class="container">
 {{--    <div class="message-blue">--}}
 {{--        <p class="message-content">現在是2021/01/22下午2:57分</p>--}}
@@ -49,7 +95,7 @@
     <button>送出</button>
 </form>
 <a href="/ta/course/{{$class}}"><i class="fas fa-arrow-left" style="color:#00a6a6"></i></a>|<a href="/ta"><i class="fas fa-home home" style="color:#00a6a6"></i></a>
-
+@endsection
 <style>
     @import url(https://fonts.googleapis.com/css?family=Open+Sans:300,400);
 
--- resources/views/ta/course.blade.php
+++ resources/views/ta/course.blade.php
@@ -1,23 +1,69 @@
-
+@extends('layouts/tahome')
 <title>{{$course_name}}-學生聯絡頁面</title>
+
+@section('tanav')
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+           aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-book"></i>
+            <span>課程</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">已選課程:</h6>
+{{--                @if ($count > 0)--}}
+{{--                    @for($i = 0; $i < $count; $i++)--}}
+{{--                        <a class="collapse-item" href="/ta/classes/@php echo $tacid[$i]; @endphp" >@php echo $tac[$i]; @endphp</a>--}}
+{{--                    @endfor--}}
+{{--                @endif--}}
+            </div>
+        </div>
+    </li>
+
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#textbooks"
+               aria-expanded="true" aria-controls="textbookss">
+                <i class="fas fa-fw fa-folder-open"></i>
+                <span>教材</span>
+            </a>
+            <div id="textbooks" class="collapse" aria-labelledby="textbooks"
+                 data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">教材相關:</h6>
+                    <a class="collapse-item" href="/textbooks/create">新增教材</a>
+                    <a class="collapse-item" href="/textbooks">教材管理</a>
+                </div>
+            </div>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+
+
+
+@endsection
+
+@section('notice')
 <div class="container">
     <h2><a href="/ta/classes/{{$classId}}"><i class="fas fa-arrow-left" style="color:#00a6a6"></i></a>{{$course_name}}<a href="/ta"><i class="fas fa-home home" style="color:#00a6a6"></i></a></h2>
     <form>
         <table align="center">
             <ul class="tatable">
-                <li class="table-header">
-                    <div class="col col-1">班級</div>
-                    <div class="col col-2"></div>
+                <li class="table-header .bg-info">
+                    <div class="col col-1"></div>
+                    <div class="col col-2 text-white">班級</div>
                     <div class="col col-3"></div>
-                    <div class="col col-4">學生姓名</div>
+                    <div class="col col-4 text-white">學生姓名</div>
                 </li>
 
                 @for($i = 0; $i < $count; $i++)
                     <li class="table-row">
-                        <div class="col col-1">@php echo $classlist[$i]; @endphp</div>
-                        <div class="col col-2"></div>
+                        <div class="col col-1"></div>
+                        <div class="col col-2">@php echo $classlist[$i]; @endphp</div>
                         <div class="col col-3"></div>
-                        <div class="col col-4"><a href="/ta/questions/@php echo $stu_id[$i]; @endphp">@php echo $student_list[$i]; @endphp</a></div>
+                        <div class="col col-4"><a href="/ta/questions/@php echo $stu_id[$i]; @endphp">@php echo $student_list[$i]; @endphp</a></div
                     </li>
                 @endfor
 
@@ -43,6 +89,7 @@
     {{--    </table>--}}
     {{--</form>--}}
 </div>
+@endsection
 <style>
     /*body {*/
     /*    display: flex;*/
